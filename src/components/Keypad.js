b872853e3ab55096b5846b4fd33531879e74d0a5
Chris Cardle <cdcardle@gmail.com>
2019-02-08 15:49:39 +0000

    Automatically backed up by Learn

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default class Keypad extends React.Component {
   render() {
     return (
-      <div>Insert Stuff Here</div>
+      <input type="password" />
     )
   }
 }
